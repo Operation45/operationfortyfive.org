70a33b6bd56294fc1cabf9ccabd5cdd0efff647b
Jeremia Kimelman <jbkimelman@gmail.com>
2017-04-26 01:46:59 -0400

    Add some design and prod stripe key

--- scripts/donate.js
+++ scripts/donate.js
@@ -1,17 +1,16 @@
 (function(window) {
-  var stripe = Stripe('pk_test_KYRURBgo7mqY51cJWF8cpQTH')
+  var stripe = Stripe('pk_live_Zctb3I1XHvRac8Fgo9jAjUpS')
   var elements = stripe.elements()
   var $form = document.querySelector('#donate-form-container')
   var $formMsg = document.querySelector('#donate-form-msg')
 
   var opts = {
-    classes: {
-      base: 'input'
-    },
     style: {
-      color: 'white',
-      fontFamily: '"PitchLight", monospace',
-      fontSize: '1.3rem'
+      base: {
+        fontFamily: 'monospace',
+        fontSize: '19.2px',
+        fontWeight: 100
+      }
     }
   }
 
@@ -27,11 +26,12 @@
   })
 
   $form.addEventListener('submit', function(ev) {
+    ev.preventDefault()
+
     var amount = $form.querySelector('#amount').value
     var email = $form.querySelector('#email').value
-    var monthly = $form.querySelector('#monthly').checked
+    var freq = $form.querySelector('[name="donate-frequency"]:checked').value
 
-    ev.preventDefault()
     stripe.createToken(card).then(function(result) {
       console.log('result', result)
       if (result.error) {
@@ -40,7 +40,7 @@
         submit({
           amount: +amount,
           email: email,
-          monthly: monthly,
+          monthly: (freq === 'monthly') ? true : false,
           token: result.token.id
         })
       }
@@ -60,7 +60,7 @@
   function showSuccess(msg) {
     $form.querySelector('#donate-form').style.display = 'none'
     $form.querySelector('input[type="submit"]').disabled = true
-    $formMsg.textContent = msg || 'Thank you for your support'
+    $formMsg.textContent = msg || 'Thank you for your support.'
   }
 
   function submit(body) {
@@ -72,6 +72,8 @@
         'Content-Type': 'application/json'
       }
     }
+
+    debugger;
 
     return fetch(api, payload).then(res => {
       if (res.status === 200) return showSuccess()
